59a6498f1d3401187fd549f088c9cab2decdecb4
AlexanderGomes <gomes.alexsander.hamir@outlook.com>
2025-04-17 11:56:32 -0700

    refactor: simplify main application with new context manager

--- main.go
+++ main.go
@@ -1,115 +1,36 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"memctx/pool"
-	"net/http"
-	_ "net/http/pprof" // Import pprof
-	"reflect"
-	"runtime"
-	"sync"
-	"time"
+	"memctx/contexts"
 )
 
+type Example struct {
+	ID int
+}
+
 func main() {
-	// enableProfiling()
+	cm := contexts.NewContextManager(nil)
 
-	// debug.SetGCPercent(-1)
+	ctx, _, err := contexts.GetOrCreateTypedPool[*Example](cm, "test", contexts.MemoryContextConfig{
+		Parent:      nil,
+		ContextType: "test",
+	})
 
-	// fmt.Println("[PPROF] Ready to profile at http://localhost:6060/debug/pprof/")
-	// time.Sleep(5 * time.Second)
-
-	// runWorkload()
-
-	// fmt.Println("[DONE] Workload finished")
-	// time.Sleep(30 * time.Second)
-
-	type User struct {
-		Name string
-		Age  int
-	}
-
-	allocator := func() *User {
-		return &User{Name: "template", Age: 42}
-	}
-
-	template := allocator()
-
-	copy := cloneUnsafe(template)
-	copy.Name = "Alex"
-	copy.Age = 120
-
-	fmt.Printf("template: %+v\n", template)
-	fmt.Printf("copy: %+v\n", copy)
-
-}
-
-func cloneUnsafe[T any](src T) T {
-	v := reflect.ValueOf(src)
-	copied := reflect.New(v.Elem().Type()).Elem()
-	copied.Set(v.Elem())
-	return copied.Addr().Interface().(T)
-}
-
-func enableProfiling() {
-	runtime.SetMutexProfileFraction(1)
-	runtime.SetBlockProfileRate(1)
-
-	go func() {
-		log.Println("[PPROF] Server running on :6060")
-		http.ListenAndServe("localhost:6060", nil)
-	}()
-}
-
-func runWorkload() {
-
-	allocator := func() *pool.Example {
-		return &pool.Example{}
-	}
-
-	cleaner := func(e *pool.Example) {
-		e.Name = ""
-		e.Age = 0
-	}
-
-	builder, err := pool.NewPoolConfigBuilder().
-		SetShrinkAggressiveness(pool.AggressivenessExtreme)
 	if err != nil {
-		log.Fatalf("Failed to set shrink aggressiveness: %v", err)
+		panic(err)
 	}
 
-	config, err := builder.Build()
+	err = ctx.CreatePool(nil,
+		func() *Example { return &Example{} },
+		func(obj *Example) { obj.ID = 0 },
+	)
+
 	if err != nil {
-		log.Fatalf("Failed to build pool config: %v", err)
+		panic(err)
 	}
 
-	poolObj, err := pool.NewPool(config, allocator, cleaner)
-	if err != nil {
-		log.Fatalf("Failed to create pool: %v", err)
-	}
+	example := ctx.Acquire()
+	example.ID = 1
 
-	numWorkers := 5
-	objectsPerWorker := 10000
-	delayBetweenTasks := 100 * time.Millisecond
-
-	log.Println("[WORKLOAD] Starting")
-
-	var innerWg sync.WaitGroup
-	for i := range numWorkers {
-		innerWg.Add(1)
-		go func(id int) {
-			defer innerWg.Done()
-			for range objectsPerWorker {
-				obj := poolObj.Get()
-				obj.Name = "user1"
-				obj.Age = 120
-				time.Sleep(50 * time.Millisecond)
-				poolObj.Put(obj)
-				time.Sleep(delayBetweenTasks)
-			}
-		}(i)
-	}
-	innerWg.Wait()
-	log.Println("[WORKLOAD] All done")
+	ctx.Release(example)
 }
